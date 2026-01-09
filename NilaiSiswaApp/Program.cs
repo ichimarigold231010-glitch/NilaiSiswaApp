70c29d25c865e20b58a8376d815d8e290aea3961
ichimarigold231010-glitch <ichimarigold231010@gmail.com>
2026-01-09 14:29:12 +0700

    menambahkan input nama dan nilai

--- NilaiSiswaApp/Program.cs
+++ NilaiSiswaApp/Program.cs
@@ -2,3 +2,10 @@
 string nama;
 
 Console.WriteLine("Aplikasi Nilai Siswa");
+
+Console.WriteLine("Nama Siswa: ");
+nama = Console.ReadLine();
+
+Console.WriteLine("Nilai: ");
+nilai = int.Parse(Console.ReadLine());
+
